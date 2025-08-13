64b4624fb117608d20979c56d5381e15d3f48cb0
JonleeJamesNavio <154415061+JonleeJamesNavio@users.noreply.github.com>
2025-08-13 16:28:49 +0800

    Add files via upload

---
+++ Covid Portfolio Project.sql
@@ -0,0 +1,128 @@
+SELECT 
+	Location, date, total_cases, new_cases, total_deaths, population
+FROM CovidDeaths
+Order BY 1,2
+
+
+
+
+SELECT 
+	Location,
+	date,
+	total_cases,
+	total_deaths,
+	(total_deaths/total_cases)*100 as DeathPercentage
+FROM CovidDeaths
+Where location = 'Philippines'
+Order BY 1,2
+
+
+
+SELECT 
+	Location,
+	date,
+	population,
+	total_cases,
+	(total_cases/population)*100 as PopulatedCases
+FROM CovidDeaths
+Where location = 'Philippines'
+Order BY 1,2
+
+
+
+
+SELECT 
+	Location,
+	population,
+	MAX(total_cases) AS HighestInfectionCount,
+	MAX((total_cases/population))*100 as PercentofPopulationInfected
+FROM CovidDeaths
+Group BY Location, population
+Order BY PercentofPopulationInfected DESC
+
+
+
+
+SELECT 
+	date,
+	SUM(new_cases) as total_cases,
+	SUM(cast(new_deaths as int)) as total_deaths,
+	SUM(cast(new_deaths as int))/SUM(new_cases)*100 as DeathPercentage
+FROM CovidDeaths
+Where continent is not null
+Group BY date
+Order BY 1,2
+
+
+
+WITH PopvsVac (Continent, Location, Date, Population, New_Vaccinations, VaccinatedPeoplePerDay)
+AS (
+SELECT 
+	dea.continent,
+	dea.location,
+	dea.date,
+	dea.population,
+	vac.new_vaccinations,
+	SUM(Convert(int, vac.new_vaccinations)) 
+	OVER (PARTITION BY dea.Location ORDER BY dea.Location, dea.Date) AS VaccinatedPeoplePerDay
+FROM CovidDeaths AS dea
+JOIN CovidVaccinations AS vac
+	ON dea.location = vac.location
+	AND dea.date = vac.date
+WHERE dea.continent is not null
+-- ORDER BY 2,3
+)
+SELECT *, (VaccinatedPeoplePerDay/Population)*100 AS VaccinatedPopulation
+FROM PopvsVac
+
+
+
+--Temp table
+
+DROP TABLE if exists #PercentPopulationVaccinated
+CREATE TABLE #PercentPopulationVaccinated
+(
+Continent nvarchar(255),
+Location nvarchar(255),
+Date datetime,
+Population numeric,
+New_Vaccinations numeric,
+VaccinatedPeoplePerDay numeric
+)
+
+Insert Into #PercentPopulationVaccinated
+SELECT 
+	dea.continent,
+	dea.location,
+	dea.date,
+	dea.population,
+	vac.new_vaccinations,
+	SUM(Convert(int, vac.new_vaccinations)) 
+	OVER (PARTITION BY dea.Location ORDER BY dea.Location, dea.Date) AS VaccinatedPeoplePerDay
+FROM CovidDeaths AS dea
+JOIN CovidVaccinations AS vac
+	ON dea.location = vac.location
+	AND dea.date = vac.date
+WHERE dea.continent is not null
+--ORDER BY 2,3
+
+SELECT *, (VaccinatedPeoplePerDay/Population)*100
+FROM #PercentPopulationVaccinated
+
+
+-- CREATING View to store data for later visualizations
+
+CREATE VIEW PercentPopulationVaccinated AS
+SELECT 
+	dea.continent,
+	dea.location,
+	dea.date,
+	dea.population,
+	vac.new_vaccinations,
+	SUM(Convert(int, vac.new_vaccinations)) 
+	OVER (PARTITION BY dea.Location ORDER BY dea.Location, dea.Date) AS VaccinatedPeoplePerDay
+FROM CovidDeaths AS dea
+JOIN CovidVaccinations AS vac
+	ON dea.location = vac.location
+	AND dea.date = vac.date
+WHERE dea.continent is not null
